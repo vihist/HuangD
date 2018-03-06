73b2398f777c52980f234d57f4bf624478828652
vihist <vihist@outlook.com>
2018-03-06 15:22:09 +0800

    add StreamManager and init process

--- Assets/Script/InitScene.cs
+++ Assets/Script/InitScene.cs
@@ -1,23 +1,5 @@
-﻿using System.IO;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using XLua;
-
-[CSharpCallLua]
-public interface PersionName
-{
-	string family { get; }
-	string given { get; }
-}
-
-[CSharpCallLua]
-public interface YearName
-{
-	string first { get; }
-	string second { get; }
-}
 
 public class InitScene : MonoBehaviour 
 {
@@ -25,7 +7,6 @@ public class InitScene : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-        LoadLua();
 
     }
 	
@@ -40,63 +21,12 @@ public class InitScene : MonoBehaviour
         Transform UIRoot = GameObject.Find("Canvas").transform.Find("Panel");
 
         InputField inPeriodName = UIRoot.transform.Find("PeriodName").Find("InputField").GetComponent<InputField>();
-        inPeriodName.text = GetRandomPeriodName();
+		inPeriodName.text = StreamManager.periodName.GetRandom ();
 
         InputField inYearName = UIRoot.transform.Find("YearName").Find("InputField").GetComponent<InputField>();
-        inYearName.text = GetRandomYearName();
+		inYearName.text = StreamManager.yearName.GetRandom();
 
         InputField inPersonName = UIRoot.transform.Find("PersonName").Find("InputField").GetComponent<InputField>();
-        inPersonName.text = GetRandomPersonName();
+		inPersonName.text = StreamManager.personName.GetRandom();
     }
-
-    private void LoadLua()
-    {
-        LuaEnv luaenv = new LuaEnv();
-        luaenv.AddLoader(CustomLoaderMethod);
-
-        luaenv.DoString("require 'name'");
-
-        personName = luaenv.Global.Get<PersionName>("person_name");
-        yearName = luaenv.Global.Get<YearName>("year_name");
-        periodName = luaenv.Global.Get<string>("period_name");
-    }
-
-    private byte[] CustomLoaderMethod(ref string fileName)
-    {
-        //找到指定文件  
-        fileName = Application.streamingAssetsPath + "/native/static/" + fileName.Replace('.', '/') + ".lua";
-        Debug.Log(fileName);
-        if (File.Exists(fileName))
-        {
-            return File.ReadAllBytes(fileName);
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    private string GetRandomPeriodName()
-    {
-        //int index = Tools.Probability.GetRandomNum(0, periodName.Count-1);
-        return periodName;
-    }
-
-    private string GetRandomYearName()
-    {
-        //int index1 = Tools.Probability.GetRandomNum(0, yearName.first.Count - 1);
-        //int index2 = Tools.Probability.GetRandomNum(0, yearName.second.Count - 1);
-        return yearName.first + yearName.second;
-    }
-
-    private string GetRandomPersonName()
-    {
-        //int index1 = Tools.Probability.GetRandomNum(0, personName.family.Count - 1);
-        //int index2 = Tools.Probability.GetRandomNum(0, personName.given.Count - 1);
-        return personName.family;
-    }
-
-    PersionName personName;
-    YearName yearName;
-    string periodName;
 }
---
+++ Assets/Script/StreamManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using XLua;
+
+public class StreamManager
+{
+	public class PeriodName
+	{
+		public PeriodName(StreamManager streamManger)
+		{
+			this.streamManager = streamManger;
+		}
+
+		public string GetRandom()
+		{
+			streamManager.LoadLua ("/native/static/", "name");
+
+			string periodName = streamManager.luaenv.Global.Get<string>("period_name");
+
+			string[] periodNameList = periodName.Split (',');
+
+			int i = Tools.Probability.GetRandomNum(0, periodNameList.Length - 1);
+			return periodNameList[i];
+		}
+
+		private StreamManager streamManager;
+	}
+
+
+	public class YearName
+	{
+		public YearName(StreamManager streamManger)
+		{
+			this.streamManager = streamManger;
+		}
+
+		public string GetRandom()
+		{
+			streamManager.LoadLua ("/native/static/", "name");
+
+			ItfYearName yearName = streamManager.luaenv.Global.Get<ItfYearName>("year_name");
+
+			string[] first = yearName.first.Split (',');
+			string[] second = yearName.second.Split (',');
+
+			int i = Tools.Probability.GetRandomNum(0, first.Length - 1);
+			int j = Tools.Probability.GetRandomNum(0, second.Length - 1);
+
+			return first[i] + second[j];
+
+		}
+
+		private StreamManager streamManager;
+	}
+
+
+	public class PersonName
+	{
+		public PersonName(StreamManager streamManger)
+		{
+			this.streamManager = streamManger;
+		}
+
+		public string GetRandom()
+		{
+			streamManager.LoadLua ("/native/static/", "name");
+
+			ItfPersonName personName = streamManager.luaenv.Global.Get<ItfPersonName>("person_name");
+
+			string[] family = personName.family.Split (',');
+			string[] given = personName.given.Split (',');
+
+			int i = Tools.Probability.GetRandomNum(0, family.Length - 1);
+			int j = Tools.Probability.GetRandomNum(0, given.Length - 1);
+
+			return family [i] + given [j];
+
+		}
+
+		private StreamManager streamManager;
+	}
+
+	private StreamManager ()
+	{
+		luaenv = new LuaEnv();
+	}
+
+	private void LoadLua(string path, string file)
+	{
+		LuaEnv.CustomLoader loader = delegate(ref string fileName) {
+			return CustomLoaderMethod(ref fileName, path);
+		};
+
+		luaenv.AddLoader (loader);
+
+		luaenv.DoString("require '"+file+"'");
+	}
+
+
+	private byte[] CustomLoaderMethod(ref string fileName, string path)
+	{
+		//找到指定文件  
+		fileName = Application.streamingAssetsPath + path + fileName.Replace('.', '/') + ".lua";
+		Debug.Log(fileName);
+		if (File.Exists(fileName))
+		{
+			return File.ReadAllBytes(fileName);
+		}
+		else
+		{
+			throw new FileNotFoundException ();
+		}
+	}
+
+
+	private static StreamManager Inst = new StreamManager();
+
+	public static PeriodName periodName = new PeriodName(Inst);
+	public static YearName yearName = new YearName(Inst);
+	public static PersonName personName = new PersonName(Inst);
+
+
+
+	private LuaEnv luaenv;
+}
+
+
+[CSharpCallLua]
+public interface ItfYearName
+{
+	string first { get; }
+	string second { get; }
+}
+
+[CSharpCallLua]
+public interface ItfPersonName
+{
+	string family { get; }
+	string given { get; }
+}
