9517ee61e0ea93d4e8ee45864eadcce3c816212d
vihist <vihist@outlook.com>
2018-03-15 10:40:55 +0800

    refactor StreamManager

--- Assets/Script/StreamManager.cs
+++ Assets/Script/StreamManager.cs
@@ -3,70 +3,56 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Tools;
 using XLua;
 
 public class StreamManager
 {
 	public class PeriodName
 	{
-		public PeriodName(StreamManager streamManger)
-		{
-			this.streamManager = streamManger;
-		}
-
 		public string GetRandom()
 		{
-			int i = Tools.Probability.GetRandomNum(0, streamManager.periodNames.Count - 1);
-			return streamManager.periodNames[i];
+			int i = Probability.GetRandomNum(0, names.Count - 1);
+			return names[i];
 		}
 
-		private StreamManager streamManager;
+		internal List<string> names = new List<string> ();
 	}
 
 
 	public class YearName
 	{
-		public YearName(StreamManager streamManger)
-		{
-			this.streamManager = streamManger;
-		}
-
 		public string GetRandom()
 		{
-			int i = Tools.Probability.GetRandomNum(0, streamManager.yearNameFirst.Count-1);
-			int j = Tools.Probability.GetRandomNum(0, streamManager.yearNameSecond.Count-1);
+			int i = Probability.GetRandomNum(0, first.Count-1);
+			int j = Probability.GetRandomNum(0, second.Count-1);
 
-			return streamManager.yearNameFirst[i] + streamManager.yearNameSecond[j];
+			return first[i] + second[j];
 		}
 
-		private StreamManager streamManager;
+		internal List<string> first = new List<string> ();
+		internal List<string> second = new List<string> ();
 	}
 
 
 	public class PersonName
 	{
-		public PersonName(StreamManager streamManger)
-		{
-			this.streamManager = streamManger;
-		}
-
 		public string GetRandom()
 		{
-			int i = Tools.Probability.GetRandomNum(0, streamManager.personNameFamily.Count - 1);
-			int j = Tools.Probability.GetRandomNum(0, streamManager.personNameGiven.Count - 1);
+			int i = Probability.GetRandomNum(0, family.Count - 1);
+			int j = Probability.GetRandomNum(0, given.Count - 1);
 
-			return streamManager.personNameFamily [i] + streamManager.personNameGiven [j];
+			return family [i] + given [j];
 		}
 
-		private StreamManager streamManager;
+		internal List<string> family = new List<string> ();
+		internal List<string> given = new List<string> ();
 	}
 
 	private StreamManager ()
 	{
         luaenv = new LuaEnv();
 		luaenv.DoString(script);
-        //LoadLua("/native/", "loader");
 
         LoadName ();
 		LoadEvent ();
@@ -74,77 +60,70 @@ public class StreamManager
 
 	private void LoadName()
 	{
+		Action<string, ItfPersonName> actionPerson = AnaylizePersonName;
+		luaenv.Global.ForEach(actionPerson);
 
-		ItfYearName yearName = luaenv.Global.Get<ItfYearName>("year_name");
-		ItfPersonName personName = luaenv.Global.Get<ItfPersonName>("person_name");
-		string speriodName = luaenv.Global.Get<string>("period_name");
+		Action<string, ItfYearName> actionYear = AnaylizeYearName;
+		luaenv.Global.ForEach(actionYear);
 
-		yearNameFirst = new List<string>(yearName.first.Split (','));
-		yearNameSecond = new List<string>(yearName.second.Split (','));
-		personNameFamily = new List<string>(personName.family.Split (','));
-		personNameGiven = new List<string>(personName.given.Split (','));
-		periodNames = new List<string>(speriodName.Split (','));
+		Action<string, string> actionPeriod = AnaylizePeriodName;
+		luaenv.Global.ForEach(actionPeriod);
 	}
 
 	private void LoadEvent()
 	{
-        eventDictionary = new Dictionary<string, ItfEvent>();
-
         Action<string, ItfEvent> action = AnaylizeEvent;
         luaenv.Global.ForEach(action);
     }
 
 	private void AnaylizeEvent(string name, ItfEvent value)
 	{
-		if (name.StartsWith ("EVENT_")) 
+		if (name.Contains ("EVENT_")) 
 		{
-			Debug.Log (name);
+			Debug.Log ("anaylize event: " + name);
 			eventDictionary.Add (name, value);
 		}
 	}
 
-	private void LoadLua(string path, string file)
+	private void AnaylizePersonName(string name, ItfPersonName value)
 	{
-		LuaEnv.CustomLoader loader = delegate(ref string fileName) {
-			return CustomLoaderMethod(ref fileName, path);
-		};
-
-		luaenv.AddLoader (loader);
-
-		luaenv.DoString("require '"+file+"'");
-	}
-
-	private byte[] CustomLoaderMethod(ref string fileName, string path)
-	{
-		//找到指定文件  
-		fileName = Application.streamingAssetsPath + path + fileName.Replace('.', '/') + ".lua";
-		Debug.Log(fileName);
-		if (File.Exists(fileName))
+		if (name.Contains ("person_name")) 
 		{
-			return File.ReadAllBytes(fileName);
-		}
-		else
-		{
-			throw new FileNotFoundException ();
+			Debug.Log ("anaylize person name: " + name);
+			personName.family.AddRange (value.family.Split (','));
+			personName.given.AddRange (value.given.Split (','));
 		}
 	}
 
+	private void AnaylizeYearName(string name, ItfYearName value)
+	{
+		if (name.Contains ("year_name")) 
+		{
+			Debug.Log ("anaylize year name: " + name);
+			yearName.first.AddRange (value.first.Split (','));
+			yearName.second.AddRange (value.second.Split (','));
+		}
+	}
 
-	private static StreamManager Inst = new StreamManager();
+	private void AnaylizePeriodName(string name, string value)
+	{
+		if (name.Contains ("period_name")) 
+		{
+			Debug.Log ("anaylize period name: " + name);
+			periodName.names.AddRange (value.Split (','));
+		}
+	}
 
-	public  static PeriodName periodName = new PeriodName(Inst);
-	public  static YearName yearName = new YearName(Inst);
-	public  static PersonName personName = new PersonName(Inst);
-	public  static Dictionary<string, ItfEvent> eventDictionary;
+	public  static PeriodName periodName = new PeriodName();
+	public  static YearName yearName = new YearName();
+	public  static PersonName personName = new PersonName();
+	public  static Dictionary<string, ItfEvent> eventDictionary = new Dictionary<string, ItfEvent>();
+
+#pragma warning disable 414  
+	private static StreamManager wInst = new StreamManager();
+#pragma warning restore
 
 	private LuaEnv luaenv;
-
-	private List<string> periodNames;
-
-	private List<string> yearNameFirst;
-	private List<string> yearNameSecond;
-	private List<string> personNameFamily;
-	private List<string> personNameGiven;
 
 	string script = @"
 		function listToTable(clrlist)
